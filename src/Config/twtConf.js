402c254034c1c70aa226118a2bb7e79ec345f080
sarmentof <fabiano@howto.com.br>
2020-02-04 18:00:35 -0300

    Ajustes em twtConfig

--- src/Config/twtConf.js
+++ src/Config/twtConf.js
@@ -1,14 +1,13 @@
 require('dotenv/config')
 const twit = require('twit')
 
-
 const Twitter = new twit ({
     consumer_key: process.env.consumer_key,
     consumer_secret: process.env.consumer_secret,
     access_token: process.env.access_token,
     access_token_secret: process.env.access_token_secret,
-    timeout_ms: process.env.timeout_ms,
-    strictSSL: process.env.strictSSL
+    timeout_ms: 60*1000,
+    strictSSL: false
     })
 
 function trackError (err, data, response) {
